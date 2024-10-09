4ce92ac686b83e658b8a1b106b63da257c105f3a
CheckerBoard <hyeong8465@naver.com>
2024-10-09 20:51:32 +0900

    [Silver I] Title: 후보 추천하기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/1713. 후보 추천하기/후보 추천하기.py
@@ -0,0 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+students = int(input())
+recommend_arr = list(map(int, input().split()))
+
+window = []
+time = 0
+for i in recommend_arr:
+    time += 1
+    if i not in [j[0] for j in window]:
+        if len(window) < n:
+            window.append([i,1,time])
+        else:
+            window.sort(key = lambda x: (-x[1],-x[2]))
+            window[-1][0] = i
+            window[-1][1] = 1
+            window[-1][2] = time
+            # print(window)
+    else:
+        for w in window:
+            if w[0] == i:
+                w[1] = w[1]+1
+    # print(window)
+answer = [w[0] for w in window]
+answer.sort()
+print(*answer)
