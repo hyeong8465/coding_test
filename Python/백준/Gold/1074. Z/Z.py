c2eb5cf9987871edd491ef6e021e9f42fb0d4f0d
CheckerBoard <hyeong8465@naver.com>
2024-12-08 13:53:53 +0000

    [Gold V] Title: Z, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Gold/1074. Z/Z.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n, r, c = map(int, input().split())
+
+def z(n, r, c):
+    if n == 0:
+        return 0
+    return 2*(r%2)+c%2 + 4*z(n-1,int(r/2),int(c/2))
+    
+print(z(n,r,c))
