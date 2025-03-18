3cbfea7eb5ee4cf8b2e05095b2bc9dbc9dae9b70
CheckerBoard <hyeong8465@naver.com>
2025-03-18 05:46:19 +0000

    [Gold V] Title: 숨바꼭질 3, Time: 168 ms, Memory: 54456 KB -BaekjoonHub

---
+++ Python/백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,34 @@
+"""
+최적화
+"""
+from collections import deque
+
+n, k = map(int, input().split())
+
+graph = [] # 0은 그냥 추가
+for i in range(0, 100002):
+    graph.append([2*i, i-1, i+1])
+visited = [False]* 100002
+visited[0] = True
+
+q = deque([(n,0)])
+if n > k:
+    print(n-k)
+    quit()
+
+while q:
+    # print(q)
+    pos, sec = q.popleft()
+    if pos == k:
+        print(sec)
+        quit()
+    for i, g in enumerate(graph[pos]):
+        if g >= 100002 or g <= -1:
+            continue
+        if not visited[g]:
+            if i == 0:
+                visited[g] = True
+                q.appendleft((g, sec))
+            else:
+                visited[g] = True
+                q.append((g, sec+1))
