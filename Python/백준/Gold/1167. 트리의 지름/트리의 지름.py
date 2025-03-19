76af517c383fc0b61f09a499b60d85c9c065eac7
CheckerBoard <hyeong8465@naver.com>
2025-03-19 06:42:30 +0000

    [Gold II] Title: 트리의 지름, Time: 544 ms, Memory: 77000 KB -BaekjoonHub

---
+++ Python/백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -0,0 +1,52 @@
+"""
+가장 긴 경로 찾기
+bfs 두 번
+아무 노드에서 가장 먼 노드 찾고 그 노드에서 가장 먼 노드 찾으면 됨
+
+"""
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+graph = [[] for _ in range(n+1)]
+while True:
+    try:
+        arr = list(map(int, input().split()))
+        s = arr[0]
+        idx = 1
+        while True:
+            if arr[idx] == -1:
+                break
+            graph[s].append((arr[idx], arr[idx+1]))
+            idx += 2
+    except:
+        break
+# print(graph)
+def bfs(start):
+    q = deque([(start, 0)])
+    visited = [False]*(n+1)
+    visited[start] = True
+    max_dis = 0
+    max_node = 0
+    
+    while q:
+        pos, dis = q.popleft()
+        for g in graph[pos]:
+            if not visited[g[0]]:
+                q.append((g[0], dis+g[1]))
+                visited[g[0]] = True
+                if max_dis < dis+g[1]:
+                    max_dis = dis+g[1]
+                    max_node = g[0]
+        # print(max_node, max_dis)
+    return max_node, max_dis
+node, val = bfs(1)
+
+print(bfs(node)[1])
+
+
+
+    
+
+
