5cfcc1c4746b37167289ed4b3a8df048c522e839
CheckerBoard <hyeong8465@naver.com>
2025-04-01 03:07:08 +0000

    [Gold III] Title: IQ Test, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Gold/1111. IQ Test/IQ Test.py
+++ Python/백준/Gold/1111. IQ Test/IQ Test.py
@@ -1,33 +1,34 @@
-def find_next_number(sequence):
-    n = len(sequence)
-    
-    if n == 1:
-        return 'A'
-    
-    if n == 2:
-        if sequence[0] == sequence[1]:
-            return sequence[0]
-        else:
-            return 'A'
-    
-    x1, x2, x3 = sequence[0], sequence[1], sequence[2]
-    
-    if x2 - x1 == 0:
-        a = 0
+# try1:
+n = int(input())
+nums = list(map(int, input().split()))
+if n == 1:
+    print('A')
+    quit()
+if n == 2:
+    if nums[0] == nums[1]:
+        print(nums[0])
+        quit()
     else:
-        if (x3 - x2) % (x2 - x1) != 0:
-            return 'B'
-        a = (x3 - x2) // (x2 - x1)
-    
-    b = x2 - a * x1
-    
-    for i in range(1, n):
-        if sequence[i] != a * sequence[i - 1] + b:
-            return 'B'
-    
-    return a * sequence[-1] + b
+        print('A')
+        quit()
 
-asfd = int(input())
-sequence = list(map(int, input().split()))
 
-print(find_next_number(sequence))  # 출력: 6
+p1 = (nums[0], nums[1])
+p2 = (nums[1], nums[2])
+
+if nums[0] == nums[1]:
+    a = 0
+else:
+    if (nums[2] - nums[1]) % (nums[1] - nums[0]) != 0:
+        print('B')
+        quit()
+    a = (p1[1]-p2[1])//(p1[0]-p2[0])
+b = p1[1]-p1[0]*a
+# print(a, b)
+
+for i in range(1,n):
+    if nums[i] != a*nums[i-1]+b:
+        print('B')
+        quit()
+
+print(int(a*nums[-1]+b))
