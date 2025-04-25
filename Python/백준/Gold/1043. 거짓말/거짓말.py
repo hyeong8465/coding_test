7cb953ab49e981d67e8a9838f774e895a040c4f3
CheckerBoard <hyeong8465@naver.com>
2025-04-25 05:59:28 +0000

    [Gold IV] Title: 거짓말, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Gold/1043. 거짓말/거짓말.py
+++ Python/백준/Gold/1043. 거짓말/거짓말.py
@@ -1,35 +1,18 @@
-# sol 1
 n, m = map(int, input().split())
-ppl = list(map(int, input().split()))
-if ppl[0] == 0:
-    print(m)
-    exit()
+knowList = set(input().split()[1:])
 
-# 진실을 알고 있는 사람
-ppl_true = set()
-for p in ppl[1:]:
-    ppl_true.add(p)
+parties = []
+for _ in range(m):
+    parties.append(set(input().split()[1:]))
 
-parties = [list(map(int, input().split())) for _ in range(m)]
+for _ in range(m):
+    for party in parties:
+        if party & knowList: # 교집합이 있다면
+            knowList = knowList.union(party) # 합집합으로 만듦
 
 cnt = 0
-party_idx = set(range(0,m))
-while True:
-    olen = len(party_idx)
-    temp_idx = set()
-    for idx in party_idx: # 파티 인덱스에 대해서
-        pos = False
-        for p in parties[idx][1:]:
-            if p in ppl_true:
-                pos = True
-                break
-        if pos: # 진실을 아는 사람이 있는 파티
-            for p in parties[idx][1:]:
-                ppl_true.add(p)
-            cnt += 1
-            temp_idx.add(idx)
-    party_idx = party_idx - temp_idx
-    nlen = len(party_idx)
-    if olen == nlen:
-        break
-print(m-cnt) 
+for party in parties:
+    if party & knowList:
+        continue
+    cnt += 1
+print(cnt)
