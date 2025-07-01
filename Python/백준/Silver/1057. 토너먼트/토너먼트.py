8762854b30d564caef10fa23c6bddae70493d802
CheckerBoard <hyeong8465@naver.com>
2025-07-01 10:55:26 +0900

    [Silver IV] Title: 토너먼트, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1057. 토너먼트/토너먼트.py
+++ Python/백준/Silver/1057. 토너먼트/토너먼트.py
@@ -1,58 +1,19 @@
-"""
-10:22
-완전 탐색
-
-토너먼트 진행 - 홀수면 마지막 번호 부전승
-
-임한수와 김지민이 만나기 전까진 계속 승리
-각 번호가 주어질때 몇라운드에서 대결?
-
-16 8 4 2
-10만명
-logN 번 경기 * N번 비교
-17번
-
-1,2,3,4,5,6
-1 3 5
-
-
-
-"""
+# 입력
 n, a, b = map(int, input().split())
 
-arr = list(range(n))
-arr[a-1] = 'a'
-arr[b-1] = 'b'
-
+# 라운드 초기화
 round = 0
-while len(arr) >= 2:
+
+# a와 b의 번호가 같아질 때까지 반복
+while a != b:
+    # 한 라운드 진행
     round += 1
-    temp = []
-    # print(len(arr))
-    for idx in range(0, len(arr)-1, 2):
-        # print(idx)
-        if arr[idx] in ('a','b'):
-            if arr[idx+1] in ('a','b'):
-                print(round)
-                quit()
-            else:
-                temp.append(arr[idx])
-        elif arr[idx+1] in ('a', 'b'):
-            temp.append(arr[idx+1])
-        else:
-            temp.append(arr[idx])
-    if len(arr) % 2 != 0:
-        temp.append(arr[-1])
-    arr = temp
-    # print(arr)
-    # print(round)
-print(-1)
+    
+    # a의 다음 라운드 번호 계산
+    a = (a + 1) // 2
+    
+    # b의 다음 라운드 번호 계산
+    b = (b + 1) // 2
 
-
-
-
-
-
-
-
-
+# 최종 라운드 출력
+print(round)
