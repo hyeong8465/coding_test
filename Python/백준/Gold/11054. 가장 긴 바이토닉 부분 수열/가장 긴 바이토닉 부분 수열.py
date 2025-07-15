56bc10416f03300c8d4f2f3c882a72da9d947437
CheckerBoard <hyeong8465@naver.com>
2025-07-15 10:32:35 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 44 ms, Memory: 34608 KB -BaekjoonHub

--- Python/백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ Python/백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -1,36 +1,31 @@
-import sys
+# bisect
 import bisect
 
-input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
 
-# 1. 왼쪽 -> 오른쪽으로, 각 원소에서 끝나는 LIS 길이 계산
-lis_dp = [0] * n
-tails = []
+lis_dp = [1]*n
+lis = []
 for i in range(n):
-    idx = bisect.bisect_left(tails, arr[i])
-    if idx == len(tails):
-        tails.append(arr[i])
+    idx = bisect.bisect_left(lis, arr[i])
+    if idx == len(lis):
+        lis.append(arr[i])
     else:
-        tails[idx] = arr[i]
+        lis[idx] = arr[i]
     lis_dp[i] = idx + 1
 
-# 2. 오른쪽 -> 왼쪽으로, 각 원소에서 시작하는 LDS 길이 계산
-lds_dp = [0] * n
-tails = []
-for i in range(n - 1, -1, -1):
-    idx = bisect.bisect_left(tails, arr[i])
-    if idx == len(tails):
-        tails.append(arr[i])
+lds_dp = [1]*n
+lds = []
+for i in range(n-1, -1, -1):
+    idx = bisect.bisect_left(lds, arr[i])
+    if idx == len(lds):
+        lds.append(arr[i])
     else:
-        tails[idx] = arr[i]
+        lds[idx] = arr[i]
     lds_dp[i] = idx + 1
 
-# 3. LIS와 LDS 길이를 합쳐서 최댓값 찾기
 max_len = 0
 for i in range(n):
-    # 각 i를 "산의 정상"으로 봤을 때의 바이토닉 수열 길이
     current_len = lis_dp[i] + lds_dp[i] - 1
     if max_len < current_len:
         max_len = current_len
