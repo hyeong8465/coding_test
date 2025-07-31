3d2f91675f6a4fe6866fc7f369da5d572364f0a4
CheckerBoard <hyeong8465@naver.com>
2025-07-31 16:29:41 +0900

    [Gold V] Title: 배, Time: 96 ms, Memory: 33432 KB -BaekjoonHub

---
+++ Python/백준/Gold/1092. 배/배.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+cranes = list(map(int, input().split()))
+m = int(input())
+cargos = list(map(int, input().split()))
+
+cranes.sort(reverse=True)
+cargos.sort(reverse=True)
+
+if cranes[0] < cargos[0]:
+    print(-1)
+else:
+    time = 0
+    while cargos:
+        time += 1
+        for crane in cranes:
+            if cargos and crane < cargos[-1]:
+                continue
+            for cargo in cargos:
+                if crane >= cargo:
+                    cargos.remove(cargo)
+                    break
+                
+    print(time)
