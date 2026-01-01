31db893d33c743361e0154bbb06d35602721e26f
CheckerBoard <hyeong8465@naver.com>
2026-01-01 13:25:14 +0900

    [Gold V] Title: 상어 초등학교, Time: 124 ms, Memory: 32544 KB -BaekjoonHub

--- Python/백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ Python/백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -1,28 +1,10 @@
-"""
-12:20
 
-인접: 상하좌우 1칸씩
+import sys
+input = sys.stdin.readline
 
-규칙
-1. 좋아하는 학생이 입전한 킨에 가장 많은 칸
-1-1. 비어있는 칸이 가장 많은 칸
-1-2. 행 번호가 가장 작은 칸 -> 열 번호가 가장 작은 칸
-
-시뮬, 구현
-
-로직
-1. 빈칸을 순회하면서 좋아하는 학생과 가장 많이 인접한 칸 탐색
-
-만족도 계산
-
-시간 복잡도 계산
-N은 최대 20
-전체 학생: N^2
-각 학생마다 전체 자리 순회: N^2
-1초 가능
-"""
 dx = [1,0,-1,0]
 dy = [0,1,0,-1]
+
 def check(stu, likes, x, y):
     like_cnt = 0
     empty_cnt = 0
@@ -39,9 +21,10 @@ def check(stu, likes, x, y):
 
 n = int(input())
 num_to_like = {}
+
 for _ in range(n**2):
     a, b, c, d, e = map(int, input().split())
-    num_to_like[a] = (b,c,d,e)
+    num_to_like[a] = set([b,c,d,e])
 
 positions = [[0]*n for _ in range(n)]
 
@@ -51,18 +34,11 @@ for stu, likes in num_to_like.items():
     pos = None
     for i in range(n):
         for j in range(n):
-            # print("stu", stu)
-            # print(i,j)
-            # print("pos", pos)
-            # print("기준:", l_cnt, e_cnt)
-
 
             if positions[i][j] != 0:
                 continue
 
             like_cnt, empty_cnt = check(stu, likes, i, j)
-            
-            # print(like_cnt, empty_cnt)
             
             # 조건 1
             if like_cnt > l_cnt:
@@ -77,9 +53,6 @@ for stu, likes in num_to_like.items():
                 pos = (i,j)
                 continue
     positions[pos[0]][pos[1]] = stu
-    # for p in positions:
-    #     print(*p)
-    # print("==="*10)
 
 # 점수 계산
 ans = 0
@@ -97,13 +70,3 @@ for i in range(n):
             continue
         ans += 10**(cnt-1)
 print(ans)
-
-            
-
-            
-            
-
-
-
-
-
