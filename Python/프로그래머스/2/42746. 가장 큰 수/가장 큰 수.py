6cf2ff0a99558448ede624b9cfbb2c67e2507799
CheckerBoard <hyeong8465@naver.com>
2026-08-17 17:45:03 +0900

    [level 2] Title: 가장 큰 수, Time: 32.14 ms, Memory: 27 MB -BaekjoonHub

--- Python/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ Python/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,5 +1,5 @@
 def solution(numbers):
     temp = [str(num) for num in numbers]
-    temp.sort(key=lambda x: x * 4, reverse=True)
+    temp.sort(key=lambda x: x * 3, reverse=True)
     answer = ''.join(temp)
     return '0' if answer[0] == '0' else answer  # 전부 0이면 맨 앞이 항상 '0'
