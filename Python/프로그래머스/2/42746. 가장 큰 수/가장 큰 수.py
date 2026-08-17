46841c74e9a630094687a4f647d1b8c22aff139a
CheckerBoard <hyeong8465@naver.com>
2026-08-17 17:36:51 +0900

    [level 2] Title: 가장 큰 수, Time: 41.20 ms, Memory: 26.7 MB -BaekjoonHub

--- Python/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ Python/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,37 +1,5 @@
-"""
-16:24
-문자열정렬
-
-1. 단순 사전식 정렬은 안됨
-3 30 34 5 9
-
-9 5 34 30 3
-9 5 34 3 30
-
-3433230
-3 30 34
-4,x,0 -> x의 기준은 앞자리수 -> 어떻게 구현?
-"""
-from functools import cmp_to_key
-
-def compare(a,b):
-    if a+b > b+a:
-        return -1
-    elif a+b<b+a:
-        return 1
-    else:
-        return 0
-
 def solution(numbers):
     temp = [str(num) for num in numbers]
-    temp.sort(key = cmp_to_key(compare))
+    temp.sort(key=lambda x: x * 4, reverse=True)
     answer = ''.join(temp)
-    nanswer = None
-    for i in range(len(answer)):
-        if answer[i] != "0":
-            nanswer = answer[i:]
-            break
-    if nanswer == None:
-        nanswer = "0"
-    
-    return nanswer
+    return '0' if answer[0] == '0' else answer  # 전부 0이면 맨 앞이 항상 '0'
