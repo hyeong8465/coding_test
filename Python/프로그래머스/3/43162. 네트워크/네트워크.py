0edeac2a66776f1483541d980484c0f0f62bce8c
CheckerBoard <hyeong8465@naver.com>
2026-08-20 23:36:20 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 11.5 MB -BaekjoonHub

--- Python/프로그래머스/3/43162. 네트워크/네트워크.py
+++ Python/프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,31 +1,26 @@
-"""
-23:06
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
 
-인접행렬
-
-
-"""
-from collections import deque
-
-def bfs(start, visited, n, computers):
-    q = deque([start])
-    visited[start] = True
-    
-    while q:
-        x = q.popleft()
-        for i, c in enumerate(computers[x]):
-            if not visited[i] and c == 1:
-                q.append(i)
-                visited[i] = True
-    return visited
-    
+def union(parent, rank, a, b):
+    ra, rb = find(parent, a), find(parent, b)
+    if ra == rb:
+        return
+    if rank[ra] < rank[rb]:
+        ra, rb = rb, ra
+    parent[rb] = ra
+    if rank[ra] == rank[rb]:
+        rank[ra] += 1
 
 def solution(n, computers):
-    answer = 0
-    visited = [False]*n
-    
+    parent = list(range(n))
+    rank = [0]*n
+
     for i in range(n):
-        if not visited[i]:
-            answer += 1
-            visited = bfs(i, visited, n, computers)
-    return answer
+        for j in range(i+1, n):
+            if computers[i][j] == 1:
+                union(parent, rank, i, j)
+
+    roots = {find(parent, i) for i in range(n)}
+    return len(roots)
