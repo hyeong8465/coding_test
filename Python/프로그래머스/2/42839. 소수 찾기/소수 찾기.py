f9f34f13ce2d665316f7cdef50ef62d56ef9744a
CheckerBoard <hyeong8465@naver.com>
2026-08-26 15:39:25 +0900

    [level 2] Title: 소수 찾기, Time: 541.80 ms, Memory: 87.8 MB -BaekjoonHub

--- Python/프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ Python/프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,28 +1,18 @@
-"""
-14:46
-
-1. 숫자 만들기 -> O(8!) -> 대략 4만건  dfs로 생성
-2. 소수 판별
-
-"""
 from itertools import permutations
 
-def check_prime(value):
-    if value in (0,1):
-        return False
-    if value in (2,3):
-        return True
-    for val in range(2, int(value**0.5)+1):
-        if value%val == 0:
-            return False
-    return True
-
 def solution(numbers):
+    limit = int('9' * len(numbers))
+    is_prime = [True] * (limit + 1)
+    is_prime[0] = is_prime[1] = False
+    for i in range(2, int(limit**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i*i, limit+1, i):
+                is_prime[j] = False
+
     answer = set()
     for i in range(1, len(numbers)+1):
         for comb in permutations(numbers, i):
             val = int("".join(comb))
-            if check_prime(val):
+            if is_prime[val]:
                 answer.add(val)
-    print(answer)
     return len(answer)
