c54d7f42f9610a6000ef764ad3472c287adb327c
CheckerBoard <hyeong8465@naver.com>
2026-08-30 15:59:27 +0900

    [level 2] Title: 모음 사전, Time: 0.67 ms, Memory: 11.3 MB -BaekjoonHub

--- Python/프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ Python/프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,32 +1,16 @@
-"""
-15:21
-AEIOU로 만들 수 있는 길이 5 이하의 모든 단어의 수 -> 약 sum(5, 5^2, 5^3, 5^4, 5^5) = 3905
-
-dfs
-
-"""
 def solution(word):
     word = list(word)
-    global answer
-    global cnt
-    answer = 0
-    cnt = 0
-    def dfs(word_list):
-        global answer
-        global cnt
-        # print(cnt, word_list)
+
+    def dfs(word_list, cnt, answer):
         if word_list == word:
             answer = cnt
-
         if len(word_list) == 5:
-            return
-
+            return cnt, answer
         for c in ["A", "E", "I", "O", "U"]:
             word_list.append(c)
-            cnt += 1
-            dfs(word_list)
+            cnt, answer = dfs(word_list, cnt+1, answer)
             word_list.pop()
+        return cnt, answer
 
-    dfs([])
-    # print(answer)
+    _, answer = dfs([], 0, 0)
     return answer
