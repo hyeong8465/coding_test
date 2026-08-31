0dc7d33e9727814ea9f7d33ec5b1b1c1f42c76bb
CheckerBoard <hyeong8465@naver.com>
2026-08-31 16:23:53 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- Python/프로그래머스/1/42862. 체육복/체육복.py
+++ Python/프로그래머스/1/42862. 체육복/체육복.py
@@ -1,29 +1,14 @@
-"""
-15:54
-1-2-3
-
-"""
-
-
 def solution(n, lost, reserve):
-    reserve_new = list(set(reserve) - set(lost))
-    reserve_new.sort()
-    lost_new = list(set(lost) - set(reserve))
-    lost_new.sort()
-    answer = n-len(lost_new)
-    print(answer)
+    lost_set = set(lost) - set(reserve)
+    reserve_set = set(reserve) - set(lost)
+    answer = n - len(lost_set)
 
-    point_lost = 0
-    point_reserve = 0
-    while point_lost < len(lost_new) and point_reserve < len(reserve_new):
-        print(point_lost, point_reserve)
-        if abs(lost_new[point_lost] - reserve_new[point_reserve]) == 1:
+    for r in sorted(reserve_set):
+        if r - 1 in lost_set:
+            lost_set.remove(r - 1)
             answer += 1
-            point_lost += 1
-            point_reserve += 1
-        elif lost_new[point_lost] > reserve_new[point_reserve]:
-            point_reserve += 1
-        elif lost_new[point_lost] < reserve_new[point_reserve]:
-            point_lost += 1
-    return answer
+        elif r + 1 in lost_set:
+            lost_set.remove(r + 1)
+            answer += 1
 
+    return answer
